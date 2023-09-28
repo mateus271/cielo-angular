f6c590e4a365cf347096f021461277cc2f8308ea
Mateus Bullerjahn <mateusfb271@gmail.com>
2023-09-27 23:20:25 -0300

    created a readme, and project server

---
+++ mock-api/src/main.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import data from "../transactions.json" assert { type: 'json' };
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/transactions", (req, res) => {
+    const searchText = req.query.search?.toLowerCase();
+    const resultTransactionSearch = data.items.filter(transaction => searchText ? transaction.name.toLowerCase().includes(searchText) : true);
+
+    // Pagination logic
+    // const pageSize = parseInt(req.query.pageSize) || 25;
+    // const pageNumber = parseInt(req.query.pageNumber) || 1;
+    // const startIndex = (pageNumber - 1) * pageSize;
+    // const endIndex = startIndex + pageSize;
+    // const paginatedData = resultTransactionSearch.slice(startIndex, endIndex);
+
+    setTimeout(() => res.json(resultTransactionSearch), resultTransactionSearch.length * 25);
+});
+
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
+});
