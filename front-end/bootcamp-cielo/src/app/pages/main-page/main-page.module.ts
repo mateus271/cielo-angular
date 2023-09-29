c8e17b217c2a172e26222bc71d31faae31f3a09d
Mateus Bullerjahn <mateusfb271@gmail.com>
2023-09-29 20:07:42 -0300

    Merge branch 'main'

--- front-end/bootcamp-cielo/src/app/pages/main-page/main-page.module.ts
+++ front-end/bootcamp-cielo/src/app/pages/main-page/main-page.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPage } from './main.page';
-import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { BarChartComponent } from 'src/app/components/bar-chart/bar-chart.component';
+import { PieChartComponent } from 'src/app/components/pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [MainPage],
+  declarations: [
+    MainPage,
+    BarChartComponent,
+    PieChartComponent
+  ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 
-export class MainPageModule { }
+export class MainPageModule {}
