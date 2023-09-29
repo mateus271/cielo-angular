2e5c927cd770ac072f43795f08a4b997d0120d02
Mateus Bullerjahn <mateusfb271@gmail.com>
2023-09-29 20:15:03 -0300

    "table-page refactor"

--- front-end/bootcamp-cielo/src/app/pages/table-page/table.page.ts
+++ front-end/bootcamp-cielo/src/app/pages/table-page/table.page.ts
@@ -1,8 +1,10 @@
 import { DatePipe } from "@angular/common";
 import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
-import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
-import { Transaction } from "src/app/interfaces/transaction.interface";
-import { ApiService } from "src/app/services/api.service";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSort, Sort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { Transaction } from "src/app/models/transaction.type";
+import { TransactionsApiService } from "src/app/services/transactions-api.service";
 
 @Component({
     selector: 'app-table-page',
@@ -11,31 +13,48 @@ import { ApiService } from "src/app/services/api.service";
     providers: [DatePipe, MatPaginatorModule]
 })
 
-export class TablePage implements OnInit {
+export class TablePage implements OnInit, AfterViewInit {
+    @ViewChild(MatSort) sort!: MatSort;
+    
     public transactions: Transaction[] = [];
+    public tableDataSource = new MatTableDataSource(this.transactions);
     public columnsToDisplay = [
-        "transactionId",
-        "transactionDate",
+        "id",
+        "date",
         "paymentType",
         "cardBrand",
         "grossAmount",
         "netAmount",
         "administrationFee",
         "channel",
-        "mdrTaxAmount",
-        "mdrFeeAmount",
-        "transactionStatus",
+        // "mdrTaxAmount",
+        // "mdrFeeAmount",
+        "status",
     ];
 
-    constructor(private apiService: ApiService, public datePipe: DatePipe) {}
+    constructor(private transactionsApiService: TransactionsApiService, public datePipe: DatePipe) {}
     
     ngOnInit(): void {
-        this.transactions = this.apiService.transactions;
     }
-    
-    // @ViewChild(MatPaginator) paginator: MatPaginator;
 
-    // ngAfterViewInit(): void {
-    //     this.dataSource.paginator = this.paginator;
-    // }
+    ngAfterViewInit(): void {
+        this.transactionsApiService.transactions.subscribe({
+            next: (transactions: Transaction[]) => {
+                this.updateTransactions(transactions);
+            }
+        })
+        if (this.transactions.length === 0) {
+            this.transactionsApiService.getTransactionsApiResponse();
+        }
+    }
+
+    public announceSortChange(event: Sort): void {
+        console.log("ordering by: " + event.active + " " + event.direction);
+    }
+
+    private updateTransactions(transactions: Transaction[]): void {
+        this.transactions = transactions;
+        this.tableDataSource = new MatTableDataSource(this.transactions);
+        this.tableDataSource.sort = this.sort;
+    }
 }
